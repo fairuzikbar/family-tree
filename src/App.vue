4de13d66a47cdd8e28cdd2ed888d15781d4c1436
fairuzikbar <fairuz.ikbar.w@mail.ugm.ac.id>
2025-04-13 20:38:47 +0700

    is it the correct router?

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/wrbt">Wrbt Fams</router-link> |
       <router-link to="/pygn">Pygn Fams</router-link>
     </nav> -->
-    <router-view />
+    <RouterView />
   </div>
 </template>
 
